3b1d87ef44a3c65e88ae285a675f9559fa69bedb
Brian Knoles <bknoles@gmail.com>
2026-02-03 16:32:29 -0500

    Telescope setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -47,4 +47,51 @@ require("lazy").setup({
   -- { "lewis6991/gitsigns.nvim", config = true }
   { "kylechui/nvim-surround", config = true },
   { "tpope/vim-fugitive" },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local telescope = require("telescope")
+      local builtin = require("telescope.builtin")
+
+      telescope.setup({
+	defaults = {
+	  layout_strategy = "vertical",
+	  layout_config = {
+	    vertical = {
+	      height = 0.6,
+	      preview_height = 0.5,
+	      preview_cutoff = 0,
+	    },
+	  },
+	},
+      })
+
+      -- Files
+      vim.keymap.set("n", "<Leader>p", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep in files" })
+      vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Grep word under cursor" })
+      vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Recent files" })
+
+      -- Buffers
+      vim.keymap.set("n", ";", builtin.buffers, { desc = "Buffers" })
+
+      -- Git
+      vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Git commits" })
+      vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Git branches" })
+      vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git status" })
+
+      -- Help
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+      vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Keymaps" })
+
+      vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
+      vim.keymap.set("n", "<leader><leader>", builtin.builtin, { desc = "Telescope pickers" })
+    end,
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+  },
 })
